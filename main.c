cdae29a6adaf3a98f70aac13c19b6e543de1dbfa
ximeng1992 <ximeng1992@163.com>
2019-06-11 23:31:51 +0800

    1. 增加新线程控制弹球的运动 2. 增加当弹球碰到挡板的运动方向改变的代码

--- main.c
+++ main.c
@@ -12,6 +12,22 @@
 
 pthread_t ntid;
 
+typedef struct
+{
+    void (*entry)(int, int, int, int); /* 函数入口*/
+    int arg[4]; /* 参数*/
+}FUNC;
+
+void *start(void *arg)
+{
+ 
+    FUNC *func = (FUNC *)arg;
+ 
+    (*(func->entry))(func->arg[0], func->arg[1], func->arg[2], func->arg[3]);
+
+    return NULL;
+}
+
 struct block 
 {
 	int W ;   //方块的宽度
@@ -25,28 +41,40 @@ struct block
     void (*move)(struct block *block, int direction) ;  //方块移动
 };
 
+struct block tmp;
+
 void show_background(void);
 void show_block(struct block block);
 void clear_block(struct block block);
 void move_block(struct block *block, int direction);
-
-void * block_move(void *arg);
+void move_ball(struct block *block, int direction);
+void * block_move(int b_W, int b_H, int b_row, int b_col);
 
 int main(void)
 {
 	int err;
 	char c;
+	int i;
 
-	struct block b1 = {4, 1, 5, 5, 1, 1, 
+	struct block b1 = {5, 1, ROW*4/5, COL/2, 1, 1, 
 					show_block, clear_block, move_block};
 	system("clear");
 	show_background();
 	printf("\033[?25l");
 	b1.show(b1);
 
-//	err = pthread_create(&ntid, NULL, block_move, NULL);
-//	if (err != 0)
-//		printf("can't create thread\n");
+	/* 构造函数集合的结构体 */
+    FUNC function = {0};
+ 
+    function.entry = block_move;
+ 
+	function.arg[0] = i;
+
+	tmp = b1;	
+
+	err = pthread_create(&ntid, NULL, start,(void *)&function);
+	if (err != 0)
+		printf("can't create thread\n");
 	
 	while (1) {
 		c = getch();
@@ -59,21 +87,25 @@ int main(void)
 			b1.clear(b1);
 			b1.move(&b1, 1);
 			b1.show(b1);
+			tmp = b1;
 			break;
 		case 'a':
 			b1.clear(b1);
 			b1.move(&b1, 2);
 			b1.show(b1);
+			tmp = b1;
 			break;
 		case 's':
 			b1.clear(b1);
 			b1.move(&b1, 3);
 			b1.show(b1);
+			tmp = b1;
 			break;
 		case 'w':
 			b1.clear(b1);
 			b1.move(&b1, 4);
 			b1.show(b1);
+			tmp = b1;
 			break;
 		default:
 			break;
@@ -84,21 +116,20 @@ int main(void)
 }
 
 void * 
-block_move(void *arg)
+block_move(int b_W, int b_H, int b_row, int b_col)
 {
 	int i;
-	struct block b1 = {4, 2, 5, 5, 1, 1, 
-					show_block, clear_block, move_block};
-	system("clear");
-	show_background();
-	printf("\033[?25l");
-	b1.show(b1);
+
+	struct block b_ball = {2, 1,  ROW*4/5-1, COL/2+2, 1, 1, 
+					show_block, clear_block, move_ball};
+
+	b_ball.show(b_ball);
 
 	while (1) {
-		b1.show(b1);
-		msleep(1000);
-		b1.clear(b1);
-		b1.move(&b1, 1);		
+		b_ball.show(b_ball);
+		msleep(200);
+		b_ball.clear(b_ball);
+		b_ball.move(&b_ball, 0);		
 	}
 }
 
@@ -125,8 +156,26 @@ void clear_block(struct block block)
 	}
 	fflush(stdout);
 }
+//移动方球
+void move_ball(struct block *block, int direction)
+{
+//	printf("======>block.row * block.col = %d x %d", block->row, block->col);
+//	printf("======>tmp.row * tmp.col = %d x %d", tmp.row, tmp.col);
+	
+	if (block->row < 3 || block->row > ROW-block->H-1)
+		block->drow = -block->drow ; 
+	if (block->col < 3 || block->col > COL-block->W-1)
+        block->dcol = -block->dcol ; 
 
-//移动方块
+	/* 碰到挡板 */
+	if (block->row >= tmp.row-tmp.H && block->col < tmp.col+tmp.W && block->col > tmp.col) {
+		block->drow = -block->drow ;
+	}
+
+	block->row += block->drow ; 
+	block->col += block->dcol ; 
+}
+//移动挡板
 void move_block(struct block *block, int direction)
 {
 	int step_row, step_col;
@@ -174,7 +223,7 @@ void move_block(struct block *block, int direction)
 		step_row = 0;
 		step_col = 0;
 	}
-	if ((block->col+step_col) < 2 || (block->col+step_col) > COL-block->H-2) {
+	if ((block->col+step_col) < 2 || (block->col+step_col) > COL-block->W) {
 		step_row = 0;
 		step_col = 0;
 	}
